06728b34253da53ad75f7adb458f6824bfa45b65
marlon-custodio <macusto@microsoft.com>
2021-07-26 16:56:18 +0100

    adicionar main e vars

---
+++ Provisionamento/vars.tf
@@ -0,0 +1,21 @@
+# Connection Azure
+variable "client_id" {
+    type = string
+    default = "e27947b0-83f1-41c6-bd23-40b9423163c4"
+}
+
+variable "client_secret" {
+    type = string
+    default = "-JS~d3Pt6MiR-647qCR3fwga9R1bhJ5eI-"
+}
+
+variable "tenant_id" {
+    type = string
+    default = "258e9552-e1c8-4e31-8f01-e490da5d558e"
+}
+
+
+variable "subscription_id" {
+    type = string
+    default = "0c15a33d-4bb6-4264-a1d8-33104603732b"
+}
